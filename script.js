fe2468a148af4ed4fda122bbbf32a38a44a5f6d4
larahbatista-wq <larah.batista@escola.pr.gov.br>
2026-09-16 13:48:32 -0300

    atualize script.js M

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+const botoesCurtir = document.querySelectorAll(".curtir");
+botoesCurtir.ForEach(function(botaoCurtir){
+    let curtir = false;
+    botaoCurtir.addEventListener("click",curtir);
+    function curtir (){
+        const contador = botaoCurtir.querySelector("span");
+        if (curtir === false)
+    }
+})
